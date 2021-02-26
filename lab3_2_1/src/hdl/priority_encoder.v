3493939e92d1469a14b4fafb46245720f8004221
Craig McMahon <cmcmahon@sgl.com>
2021-02-26 10:46:26 -0500

    Fix broken priority encoder

--- lab3_2_1/src/hdl/priority_encoder.v
+++ lab3_2_1/src/hdl/priority_encoder.v
@@ -21,60 +21,60 @@
 
 
 module priority_encoder(
-    input wire [0:7] x, // input number
+    input wire [7:0] x, // input number
     input wire en_in_n, 
     output reg [2:0] y,
     output reg gs, // everything high
     output reg en_out // enable out
     );
 
-always begin
-    if (en_in_n == 1) begin
+always @( x, en_in_n ) begin
+    if (en_in_n) begin
         // input disabled
-        y = 0'h7;
+        y = 3'h7;
         gs = 1;
         en_out = 1;
     end // if ~en_in_n
 
     else
-        case (x)
+        casex (x)
             8'b11111111: begin
                 y = 0;
                 gs = 1;
                 en_out = 0;
             end
             8'bx1111111: begin
-                y = 0'b111;
+                y = 3'b111;
                 gs = 0;
                 en_out = 1;
             end
             8'bxx111111: begin
-                y = 0'b110;
+                y = 3'b110;
                 gs = 0;
                 en_out = 1;
             end
             8'bxxx11111: begin
-                y = 0'b101;
+                y = 3'b101;
                 gs = 0;
                 en_out = 1;
             end
             8'bxxxx1111: begin
-                y = 0'b100;
+                y = 3'b100;
                 gs = 0;
                 en_out = 1;
             end
             8'bxxxxx111: begin
-                y = 0'b011;
+                y = 3'b011;
                 gs = 0;
                 en_out = 1;
             end
             8'bxxxxxx11: begin
-                y = 0'b010;
+                y = 3'b010;
                 gs = 0;
                 en_out = 1;
             end
             8'bxxxxxxx1: begin
-                y = 0'b001;
+                y = 3'b001;
                 gs = 0;
                 en_out = 1;
             end
